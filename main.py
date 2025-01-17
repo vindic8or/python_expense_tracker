68c822f0183586ced91e2f28d80a52872f1c8933
Algirdas Varnagiris <varnagiris.algirdas@gmail.com>
2025-01-17 03:22:16 +0000

    added header and footer, comment structure, greeting

--- main.py
+++ main.py
@@ -1,1 +1,82 @@
-print("hello world")
+
+# ? Boiler plate. Formatting Variables
+format_output = "\033[47m\033[30m"
+format_reset = "\033[0m"
+
+# Formatted Message - Signify Start of Output
+print(f"{format_output}---START---{format_reset}")
+#------------------------------------------------------
+
+#?-----------------1 User name input, and greeting.
+user = "Guest"
+# input("Please enter username: ")
+print(f"Hello, {user}. \nExpenses aren't fun. \nLet me help you.")
+
+#?-----------------2 Expense logging
+
+def add_new_expense(amount, category, description):
+  print("test")
+
+
+
+
+add_new_expense(1,2,3)
+
+
+# *under construction
+# print(2+2)
+testvar = print(1+2)
+
+
+# testdict = {
+
+# }
+
+
+
+
+
+
+#?-------
+
+
+
+
+expenses = {
+  
+}
+
+first_expense = {
+  "amount": 21.93,
+  "category" : "food",
+  "description": ["fruit", "veg", "snacks"]
+}
+
+expenses.update()
+
+print(expenses)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------
+# ? Boiler plate. Formatted Message - Signify End of Output
+print(f"{format_output}---END---{format_reset}")
